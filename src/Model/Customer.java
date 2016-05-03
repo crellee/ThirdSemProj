c4821d613ac10000f0f52d34afdb57a3cabebde2
stinaanita <stina-stud@live.dk>
2016-05-03 13:08:35 +0200

    employee og customer class

---
+++ src/Model/Customer.java
@@ -0,0 +1,63 @@
+package Model;
+
+/**
+ * Created by kristinOlof on 03-05-2016.
+ */
+public class Customer
+{
+    private String firstName;
+    private String lastName;
+    private int zipCode;
+    private String email;
+
+    public Customer()
+    {}
+
+    public Customer(String firstName, String lastName, int zipCode, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.zipCode = zipCode;
+        this.email = email;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public int getZipCode()
+    {
+        return zipCode;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setZipCode(int zipCode)
+    {
+        this.zipCode = zipCode;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
---
+++ src/Model/Emloeyy.java
@@ -0,0 +1,29 @@
+package Model;
+
+/**
+ * Created by kristinOlof on 03-05-2016.
+ */
+public class Emloeyy
+{
+    private String name;
+    private int id;
+
+    public Emloeyy()
+    {}
+
+    public Emloeyy(String name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
