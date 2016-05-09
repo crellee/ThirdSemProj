02852f458315acb51438a60e5a348a562e0293d4
stinaanita <stina-stud@live.dk>
2016-05-09 13:52:29 +0200

    dummie info

--- src/Database/DummieInfo.java
+++ src/Database/DummieInfo.java
@@ -10,43 +10,40 @@ public class DummieInfo
 {
     public void customerInfo()
     {
-        String sqlString, sqlString1, sqlString2;
-        Statement stmt, stmt1, stmt2;
+        String sqlString, sqlString1, sqlString2, sqlString3;
+        Statement stmt, stmt1, stmt2, stmt3;
 
         try {
             Connection conn = DBConnection.getConnection();
             stmt = conn.createStatement();
 
             sqlString ="INSERT INTO customer (customerId, customerFirstName, customerLastName, email, zipCode)" +
-                    "VALUES( 15 ,'  Hanne ',' Frank ',' hanne@live.dk ', 2500 );" +
-                    "INSERT INTO imerco_project.customer(customerId, customerFirstName, customerLastName, email, zipCode)\n" +
-                    "VALUES(11, 'Bo', 'Gremling', 'bobo@gmail.dk', 2200);" +
-                    "INSERT INTO imerco_project.customer(customerId, customerFirstName, customerLastName, email, zipCode)\n" +
-                    "VALUES(5, 'Jarla', 'Nilsen', 'jn@mail.dk', 3500);";
+                    "VALUES( 15 ,'  Hanne ',' Frank ',' hanne@live.dk ', 2500 )," +
+                    "(11, 'Bo', 'Gremling', 'bobo@gmail.dk', 2200)," +
+                    "(5, 'Jarla', 'Nilsen', 'jn@mail.dk', 3500);";
 
 
             stmt.executeUpdate(sqlString);
 
             stmt1 = conn.createStatement();
             sqlString1 ="INSERT INTO employee (employeeId, employeeName)" +
-                    "VALUES(25,'Josefine');" +
-                    "INSERT INTO imerco_project.employee(employeeId, employeeName)" +
-                    "VALUES(36,'Andreas');" +
-                    "INSERT INTO imerco_project.employee(employeeId, employeeName)" +
-                    "VALUES(14,'Freja');";
+                    "VALUES(25,'Josefine'),(36,'Andreas'),(14,'Freja');";
 
             stmt1.executeUpdate(sqlString1);
 
             stmt2 = conn.createStatement();
             sqlString2 = "INSERT INTO imercocard(imercoCardId, customerId, point)"+
-                    "VALUES(5, 15, 12);"+
-                    "INSERT INTO imerco_project.imercocard(imercoCardId, customerId, point)"+
-                    "VALUES(8, 11, 225);"+
-                    "INSERT INTO imerco_project.imercocard(imercoCardId, customerId, point)"+
-                    "VALUES(6, 5, 55);";
+                    "VALUES(5, 15, 12),(8, 11, 225),(6, 5, 55);";
 
             stmt2.executeUpdate(sqlString2);
 
+            stmt3 = conn.createStatement();
+            sqlString3 = "INSERT INTO products(productId, productName, productDescription, price, discount)"+
+                    "VALUES(15, 'Vase', 'Vase med guldflager', 3999, NULL), " +
+                    "(1545, 'Teppe', 'Cotton fluffy', 299, NULL)" +
+                    "(4578, 'Beslag', 'Messingbeslag rustfrit', 159, NULL);";
+
+            stmt3.executeUpdate(sqlString3);
         }
         catch (Exception e)
         {
