8b324a809c86fee941b16fc9217e9524808aa4f9
christianhasselstrom <crellecrelle@gmail.com>
2016-05-09 23:07:21 +0200

    Homepage in the making nabs pt. 3

---
+++ src/GUI/GUI_Controller/HomePage_Controller.java
@@ -0,0 +1,28 @@
+package GUI.GUI_Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by christianhasselstrom on 09/05/2016.
+ */
+public class HomePage_Controller implements Initializable
+{
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
+
+    public void addToTable()
+    {
+
+    }
+}
