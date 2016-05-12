d961848d3e5de75b2f939def34ac35f86819407a
christianhasselstrom <crellecrelle@gmail.com>
2016-05-12 09:25:20 +0200

    Homepage in the making nabs pt. 4 + dummy data rettelser

---
+++ src/Database/ProductVerifier.java
@@ -0,0 +1,20 @@
+package Database;
+
+import java.sql.Connection;
+
+/**
+ * Created by christianhasselstrom on 12/05/2016.
+ */
+public class ProductVerifier
+{
+    public static void verifyProduct()
+    {
+        String productStr;
+
+       /* try
+        {
+            Connection
+
+        }*/
+    }
+}
--- src/GUI/GUI_Controller/HomePage_Controller.java
+++ src/GUI/GUI_Controller/HomePage_Controller.java
@@ -3,6 +3,7 @@ package GUI.GUI_Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +16,9 @@ public class HomePage_Controller implements Initializable
 
 
 
+    @FXML
+    private TextField textField1;
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
@@ -23,6 +27,8 @@ public class HomePage_Controller implements Initializable
 
     public void addToTable()
     {
+
+        System.out.println(textField1.getText().toString());
 
     }
 }
