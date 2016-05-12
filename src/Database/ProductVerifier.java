6ef2521abd07cf8be28467fa232b73e28e682631
christianhasselstrom <crellecrelle@gmail.com>
2016-05-12 10:14:49 +0200

    Homepage in the making nabs pt. 5

--- src/Database/ProductVerifier.java
+++ src/Database/ProductVerifier.java
@@ -1,20 +1,58 @@
 package Database;
 
+import GUI.GUI_Controller.HomePage_Controller;
+import javafx.scene.control.TextField;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * Created by christianhasselstrom on 12/05/2016.
  */
 public class ProductVerifier
 {
-    public static void verifyProduct()
+    public static void verifyProduct(TextField str)
     {
-        String productStr;
+        String productStr = str.getText().toString();
 
-       /* try
+        try
         {
-            Connection
+            Connection conn = DBConnection.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs;
 
-        }*/
+            if(productStr != null)
+            {
+                String sqlString = "SELECT * FROM Imerco_Project.Products WHERE productId = '"+productStr+"' ";
+                rs = stmt.executeQuery(sqlString);
+                if(rs.next())
+                {
+                    HomePage_Controller.addToTable2();
+                }
+            }
+
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+    public static ResultSet tableShow(TextField str)
+    {
+        String productStr = str.getText().toString();
+        ResultSet rs = null;
+        try
+        {
+            Connection conn = DBConnection.getConnection();
+            String sqlString = "SELECT * FROM Products WHERE productId = '"+productStr+"' ";
+            rs = conn.createStatement().executeQuery(sqlString);
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        return rs;
     }
 }
--- src/GUI/GUI_Controller/HomePage_Controller.java
+++ src/GUI/GUI_Controller/HomePage_Controller.java
@@ -1,9 +1,15 @@
 package GUI.GUI_Controller;
 
+import Database.ProductVerifier;
+import Model.Product;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +24,16 @@ public class HomePage_Controller implements Initializable
 
     @FXML
     private TextField textField1;
+    @FXML
+    static TableColumn prodId;
+    @FXML
+    static TableColumn desc;
+    @FXML
+    static TableColumn price;
+    @FXML
+    static TableColumn disc;
+    @FXML
+    static TableColumn amount;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
@@ -29,6 +45,20 @@ public class HomePage_Controller implements Initializable
     {
 
         System.out.println(textField1.getText().toString());
+        ProductVerifier.verifyProduct(textField1);
+
+    }
+    public static void addToTable2()
+    {
+
+        prodId.setCellValueFactory(new PropertyValueFactory<Model.Product, Integer>("productId"));
+        desc.setCellValueFactory(new PropertyValueFactory<Model.Product, String>("productDescription"));
+        price.setCellValueFactory(new PropertyValueFactory<Model.Product, Double>("price"));
+        disc.setCellValueFactory(new PropertyValueFactory<Model.Product, Integer>("discount"));
+        amount.setCellValueFactory(new PropertyValueFactory<Model.Product, Integer>("amount"));
+
+        ObservableList<Product> data = FXCollections.observableArrayList();
+
 
     }
 }
