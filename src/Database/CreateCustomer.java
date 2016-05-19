e301bacf52bb8f7d005d6783d180eef02828e718
christianhasselstrom <crellecrelle@gmail.com>
2016-05-19 14:17:53 +0200

    Homepage in the making nabs pt. 26 (Customer)

--- src/Database/CreateCustomer.java
+++ src/Database/CreateCustomer.java
@@ -10,12 +10,13 @@ import java.sql.Statement;
  */
 public class CreateCustomer
 {
-    public static void createCustomer(TextField fname, TextField lname, TextField email, TextField zip)
+    public static boolean createCustomer(TextField fname, TextField lname, TextField email, TextField zip)
     {
         String fnameStr = fname.getText().toString();
         String lnameStr = lname.getText().toString();
         String emailStr = email.getText().toString();
         int zipInt = Integer.parseInt(zip.getText().toString());
+        boolean succesfull = false;
 
         try
         {
@@ -25,9 +26,25 @@ public class CreateCustomer
             String sqlString = ("INSERT INTO Customer (customerFirstName, customerLastName, email, zipCode) " +
                     "VALUES ('"+fnameStr+"' , '"+lnameStr+"' , '"+emailStr+"' , '"+zipInt+"'  ) ");
             stmt.executeUpdate(sqlString);
+            succesfull = true;
+
         }
         catch (Exception e)
         {
+
+        }
+        return succesfull;
+    }
+
+    public static void getCustomerId(TextField email)
+    {
+        String emailStr = email.getText().toString();
+        try
+        {
+            Connection conn = DBConnection.getConnection();
+            Statement stmt = conn.createStatement();
+
+            String sqlString = ("SELECT CustomerId FROM Customer WHERE email = '"+emailStr+"' ");
 
         }
     }
